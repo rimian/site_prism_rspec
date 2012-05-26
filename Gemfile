da8ad3167d01f090e6c47aa5ba9edb49bffb1055
Rimian Perkins <hello@rimian.com.au>
2012-05-26 23:27:01 +1000

    new rails

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.1'
+
+gem 'sqlite3'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'capybara', :git => 'git://github.com/rimian/capybara.git', :branch => 'host_port'
+  gem 'launchy'
+  gem 'database_cleaner'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+gem 'jquery-rails'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SitePrismRails::Application.config.secret_token = '156f3afe4ee995d10eea3d9f4a49a2067855b352b74ae9f4304d0121f7fc19a966a705086b76f4ea9c5e4b50193912942d100e369b60937d36cc1faa1de66a08'
